2d943eb46a167d0c21ba398f15c6734396866686
Bugs Bunny <Cristian.e.cuevas707@gmail.com>
2021-08-17 21:20:27 -0400

    Added objects to day and exercises workoutSchema

--- models/workout.js
+++ models/workout.js
@@ -2,11 +2,42 @@ const mongoose = require('mongoose');
 
 const workoutSchema = new workoutSchema({
     day: {
-
+        type: Date,
     },
-    exercises: {
+    exercises:
+        [
+            {
+                type: {
+                    type: String,
+                    required: "Enter an exercise type"
+                },
+                name: {
+                    type: String,
 
-    }
+                },
+                weight: {
+                    type: Number,
+
+                },
+                sets: {
+                    type: Number,
+
+                },
+                reps: {
+                    type: Number,
+
+                },
+                duration: {
+                    type: Number,
+
+                },
+                distance: {
+                    type: Number,
+
+                },
+            }
+        ]
+
 })
 
 const Workout = mongoose.model("Workout", workoutSchema);
