6b38bdd4dc5737c77dd55f49779311f4ba8d4052
Bugs Bunny <Cristian.e.cuevas707@gmail.com>
2021-08-19 18:11:49 -0400

    Fixed schema not being able to be read

--- models/workout.js
+++ models/workout.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema
 
-const workoutSchema = new workoutSchema({
+const workoutSchema = new Schema({
     day: {
         type: Date,
     },
